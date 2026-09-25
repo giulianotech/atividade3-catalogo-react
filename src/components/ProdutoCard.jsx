31f240a6cb531093842a7eb32994d2afda301308
Giuliano Mota Pereira <giulimp2@gmail.com>
2026-09-25 15:34:04 -0300

    O arquivo foi atualizado

--- src/components/ProdutoCard.jsx
+++ src/components/ProdutoCard.jsx
@@ -1,24 +1,35 @@
-export function ProdutoCard({nome, preco, categoria,emPromocao}) {
+export function ProdutoCard({nome, preco, categoria,emPromocao, children}) {
     return (
         <div style={{
             border: '1px solif #646cff',
             padding: '20px',
             borderRadius: '8px',
             width: '250px',
-            background: '#1a1a1a'
-        }}>
-            <h3 style={{margin: '0 0 10px 0'}}>{nome}</h3>
-            <p style={{margin: '5px 0' }}>Categoria:{categoria}</p>
-            <p style={{margin: '5px 0', fontSize: '1.2em'}}>
+            backgroundColor: '#1a1a1a',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+
+            }}>
+            <div>
+                <h3 style={{margin: '0 0 10px 0'}}>{nome}</h3>
+                <p style={{margin: '5px 0' }}>Categoria:{categoria}</p>
+                <p style={{margin: '5px 0', fontSize: '1.2em'}}>
                 <strong>R${preco.toFixed(2)}</strong>
             </p>
-
-            {/* Condicional JSX com Operador Ternário */}
-            {emPromocao ? (
+                 {/* Condicional JSX com Operador Ternário */}
+                 {   emPromocao ? (
                 <span stylr={{ color: '#4ade80', fontWeight: 'bold' }}>Em Promoção</span>
-            ) : (
-                <span stylr={{ color: '#a1a1aa' }}>Preço normal</span>
-            )}                                                      
-     </div>
+                ) : (
+                <span stylr={{ color: '#a1a1aa' }}>Preço Normal</span>
+                )}                                                      
+            </div>
+
+            {/*rederização da propriedade especial children (ex: Botão comprar ou Remover) */}
+             <div style={{marginTop: '15px'}}>
+               {children}
+             </div>
+
+        </div>
 );
 }
